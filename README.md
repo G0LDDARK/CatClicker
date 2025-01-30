e6691053f0949a04a6806f3a8be4054d8bd758f8
G0LDDARK <g0lddark.linux@gmail.com>
2025-01-30 18:47:25 +0100

    0.0.3: Global optimisation, cf README

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # CatClicker
-<img src="assets/pastleCat.png" alt="The cute cat you should click on!"> <br>
+<img src="assets/pastle_cat.png" alt="The cute cat you should click on!"> <br>
 CatClicker is a cookie clicker-like game, but it's cats programmed in JavaSrcipt <br>
 Last "playable" update can be played at https://catclicker.glddrk.fr <br>
 Thanks for playing and enjoy!
 
 ## Changelog:
 
+### 0.0.3 (30 JAN 25)
+#### Major changes:
+-Refactorisation
+    -Created buy function to have a reusable function
+    -Changed img name
+
+-Removed little anti-cheat:
+    -Would probably never be brought back up
+
 ### 0.0.2 (09 NOV 24)
-#### Majors changes :
+#### Major changes:
 - Adding "cuddle" item: gives LP per second
     - LVL 1 price: 50 LP,
     - LVL 1 LP/s: 1 LP/s.
@@ -17,6 +26,6 @@ Thanks for playing and enjoy!
 
 - Changing prices factor.
 
-#### Minors changes :
+#### Minor changes:
 - Some code refactoring all around the project.
 - Starting with 10 LP instead of 1.
--- js/app.js
+++ js/app.js
@@ -1,70 +1,73 @@
 //Alpha Alert
-//alert("This game is a side project. It's still in early development phase, thus, it might be buggy and (for now) no progress can be saved!\nThanks for playing and enjoy!")
+alert("This game is a side project. It's still in early development phase, thus, it might be buggy and (for now) no progress can be saved!\nThanks for playing and enjoy!")
 
-//LOVE POINTS
+//Variable initialisation
+//Love points related
 let lp = document.querySelector('#lp_count');
-let parsed_lp = parseFloat(lp.innerHTML);
-
-//LOVE PONTS PER SECONDE
 let lpps = document.querySelector('#lpps');
+let parsed_lp = parseFloat(lp.innerHTML);
 let parsed_lpps = parseFloat(lpps.innerHTML);
 
-//CLICKERS
+//Clicker related
 let click_force_cost = document.querySelector('#click_force_cost');
-let parsed_click_force_cost = parseFloat(click_force_cost.innerHTML);
 let click_force_lvl = document.querySelector('#click_force_lvl');
+let parsed_click_force_cost = parseFloat(click_force_cost.innerHTML);
 let parsed_click_force_lvl = parseFloat(click_force_lvl.innerHTML);
 
-//CUDDLE
+//Cuddle related
 let cuddle_power_cost = document.querySelector("#cuddle_power_cost");
-let parsed_cuddle_power_cost = parseFloat(cuddle_power_cost.innerHTML);
 let cuddle_power_lvl = document.querySelector("#cuddle_power_lvl");
+let parsed_cuddle_power_cost = parseFloat(cuddle_power_cost.innerHTML);
 let parsed_cuddle_power_lvl = parseFloat(cuddle_power_lvl.innerHTML);
 
-function incrementLP()
+//Pricipals functions
+//Click on the cat
+function cat_clicked()
 {
     parsed_lp = parsed_lp + (1 * parsed_click_force_lvl);
     lp.innerHTML = parsed_lp.toFixed(2);
 }
 
-function buyClicker()
+function check_buyable(to_buy)
 {
-    if (parsed_lp >= parsed_click_force_cost) {
-        parsed_lp -= parsed_click_force_cost;
-        lp.innerHTML = parsed_lp.toFixed(2);
-
-        parsed_click_force_lvl = parsed_click_force_lvl * Math.log(2.9);
-        click_force_lvl.innerHTML = parsed_click_force_lvl.toFixed(2);
-
-        parsed_click_force_cost = parsed_click_force_cost * Math.log(3.1);
-        click_force_cost.innerHTML = parsed_click_force_cost.toFixed(2);
-    }
+    if (parsed_lp >= to_buy)
+        return 1;
+    else
+        return 0;
 }
 
-function buyCuddle()
+//Buy function
+function buy(str)
 {
-    if (parsed_lp >= parsed_cuddle_power_cost) {
-        parsed_lp -= parsed_cuddle_power_cost;
-        lp.innerHTML = parsed_lp.toFixed(2);
-
-        if (parsed_cuddle_power_lvl == 0) {
-            parsed_cuddle_power_lvl += 1;
-        } else {
-            parsed_cuddle_power_lvl = parsed_cuddle_power_lvl * Math.log(3);
+    if (str == 'clicker') {
+        if (check_buyable(parsed_click_force_cost) == 1) {
+            parsed_lp -= parsed_click_force_cost;
+            lp.innerHTML = parsed_lp.toFixed(2);
+            parsed_click_force_lvl = parsed_click_force_lvl * Math.log(2.9);
+            click_force_lvl.innerHTML = parsed_click_force_lvl.toFixed(2);
+            parsed_click_force_cost = parsed_click_force_cost * Math.log(3.1);
+            click_force_cost.innerHTML = parsed_click_force_cost.toFixed(2);
         }
-        cuddle_power_lvl.innerHTML = parsed_cuddle_power_lvl.toFixed(2);
-
-        parsed_cuddle_power_cost = parsed_cuddle_power_cost * Math.log(3.1);
-        cuddle_power_cost.innerHTML = parsed_cuddle_power_cost.toFixed(2);
-
-        parsed_lpps = parsed_cuddle_power_lvl;
-        lpps.innerHTML = parsed_lpps.toFixed(2);
+    } else if (str == 'cuddle') {
+        if (check_buyable(parsed_cuddle_power_cost) == 1) {
+            parsed_lp -= parsed_cuddle_power_cost;
+            lp.innerHTML = parsed_lp.toFixed(2);
+            if (parsed_cuddle_power_lvl == 0)
+                parsed_cuddle_power_lvl += 1;
+            else
+                parsed_cuddle_power_lvl = parsed_cuddle_power_lvl * Math.log(3);
+            cuddle_power_lvl.innerHTML = parsed_cuddle_power_lvl.toFixed(2);
+            parsed_cuddle_power_cost = parsed_cuddle_power_cost * Math.log(3.1);
+            cuddle_power_cost.innerHTML = parsed_cuddle_power_cost.toFixed(2);
+            parsed_lpps = parsed_cuddle_power_lvl;
+            lpps.innerHTML = parsed_lpps.toFixed(2);
+        }
     }
 }
 
-setInterval(() => 
+//Clock
+setInterval(() =>
 {
-    little_anti_cheat();
     if (parsed_lpps > 0) {
         parsed_lp += parsed_lpps / 10;
         lp.innerHTML = parsed_lp.toFixed(2);
